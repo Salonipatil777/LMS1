122534d14402d0e17f02559d7d9a37bfd08a6d40
Salonipatil777 <116019571+Salonipatil777@users.noreply.github.com>
2023-07-25 15:47:39 +0530

    Update urls.py

--- LMS/urls.py
+++ LMS/urls.py
@@ -46,4 +46,7 @@ urlpatterns = [
     path('course/watch-course/<slug:slug>',views.WATCH_COURSE,name='watch_course'),
     path('verify_payment',views.VERIFY_PAYMENT,name='verify_payment'),
 
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
