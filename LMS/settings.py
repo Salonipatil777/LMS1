7120092162ed3921d9e5a65c2c008a4bc1fd2b95
Salonipatil777 <116019571+Salonipatil777@users.noreply.github.com>
2023-07-25 12:20:38 +0530

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y!lxh1#lgv@1o%v0h#@znah--1nr12*e_(wot&hs$s%ujxqqn=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-89b35.up.railway.app/']
 
 
 # Application definition
